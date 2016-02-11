1c92c873dae632721a88d823333210c65bf38728
amir0080 <amirabas0080@gmail.com>
2016-02-11 12:23:30 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {amir00800},
+    sudo_users = {125658049},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
